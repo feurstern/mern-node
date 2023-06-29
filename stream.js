732242add823994a3cab671b4cc6e20e30a575c7
Rio <naziincolour123@gmail.com>
2023-06-29 13:04:37 +0700

    make some changes to wrting and reading the file by using data stream

--- stream.js
+++ stream.js
@@ -1,5 +1,13 @@
-const fs  = require('fs');
+const fs = require('fs')
 const path = require('path')
 
-const rs =  fs.createReadStream();
 
+//reaad tjhe file that we want to open
+const rs = fs.createReadStream(path.join(__dirname, 'writingfile', 'diary.txt'), {encode : 'utf8'});
+//create the file.
+const ws = fs.createWriteStream(path.join(__dirname, 'writingfile', 'new-diary.txt'));
+
+rs.on('data', (chunkData)=>{
+    ws.write(chunkData);
+    console.log(chunkData.toString())
+})
